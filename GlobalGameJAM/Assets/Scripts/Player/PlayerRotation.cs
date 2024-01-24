f7056eb6b747356aa95ce6206c999e4e51226161
morgan2334 <44641317+morgan2334@users.noreply.github.com>
2024-01-24 14:47:00 +0000

    bullet logic

---
+++ GlobalGameJAM/Assets/Scripts/Player/PlayerRotation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    public float Sensitivity = 100f;
+    float xRot;
+    public Transform playerBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * Sensitivity * Time.deltaTime;
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
