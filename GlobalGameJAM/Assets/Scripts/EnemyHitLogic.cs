899e95253b3503c98c5416dc851af55498416dae
morgan2334 <44641317+morgan2334@users.noreply.github.com>
2024-01-25 14:46:56 +0000

    funny stuff

---
+++ GlobalGameJAM/Assets/Scripts/EnemyHitLogic.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyHitLogic : MonoBehaviour
+{
+    public float EnemyHealth;
+    public Rigidbody This;
+    int speed;
+    int UpSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemyHealth = 100f;
+        speed = 10;
+        UpSpeed = 1000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("PlayerBullet"))
+        {
+            This.AddForce(other.transform.forward * speed);
+            This.AddForce(transform.up * UpSpeed);
+        }
+    }
+}
